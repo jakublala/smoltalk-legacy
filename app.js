a7f1e7a6fc5bb07b1ddc4b378c5d7642524a77f5
jakublala <jakublala@gmail.com>
2023-03-30 10:30:46 +0100

    trying to feed active code back to LLM

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.static(__dirname, {
 const configuration = new Configuration({
   apiKey: apiKey,
 });
+
 const openai = new OpenAIApi(configuration);
 
 // Handle the API request
@@ -45,24 +46,41 @@ app.get('/api/generate', async (req, res) => {
     try {
         // Get user input from query string
         let userInput = req.query.prompt;
-        // userInputs.push(userInput)
-        let prompt = createPrompt(userInput)
-        // Call OpenAI API with user input
-        const response = await openai.createChatCompletion({
-            model: 'gpt-3.5-turbo',
-            messages: [{
-                role: 'user',
-                content: prompt,
-            }],
-            max_tokens: 512,
-            n: 1,
-            });
-        // Sending the model output to the client
-        modelOutput = response.data.choices[0].message.content
-        modelOutputs.push(modelOutput)
-        res.send(modelOutput);
+        let systemPrompt = fs.readFileSync('prompts/openAI.txt', 'utf8');
+        let code, lines;
+        
+        // if activeCode does not exist, read code.txt
+        if (activeCode == null) {
+          code = fs.readFileSync('prompts/code.txt', 'utf8');
+        } else {
+          lines = activeCode.split('\n');
+          code = fs.readFileSync('prompts/code.txt', 'utf8');
+          lines = code.split('\n');
+          // add active code above [INSERT CODE HERE]
+          lines.splice(lines.indexOf('[INSERT CODE HERE]') - 1, 0, activeCode);
+          code = lines.join('\n');
+        }
 
-        // res.send(response.data.choices[0].message.content);
+        let prompt = systemPrompt + code + userInput;;
+
+        // Call OpenAI API with user input
+        try {
+          const response = await openai.createChatCompletion({
+              model: 'gpt-3.5-turbo',
+              messages: [{
+                  role: 'user',
+                  content: prompt,
+              }],
+              max_tokens: 512,
+              n: 1,
+          });
+          activeCode = response.data.choices[0].message.content;
+          res.send(activeCode);
+          console.log(activeCode)
+          } catch (error) {
+              console.error(error);
+          }
+
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal server error');
